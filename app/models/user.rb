361228169d283af75d377ed35e681a7ab1ab8b84
Muck023 <ker2job9@gmail.com>
2022-07-20 20:50:57 +0900

    profile_imageフォームの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :profile_img
+  has_one_attached :profile_image
 
   validates :nickname, presence: true
 end
