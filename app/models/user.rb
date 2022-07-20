4f455d23265ff9e1767778c4e58c99074e561664
Muck023 <ker2job9@gmail.com>
2022-07-20 21:22:40 +0900

    エラーメッセージを日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
 
-  validates :nickname, presence: true
+
 end
