c6e3c938c4f7288a03a0d678f00669023eb07f70
Muck023 <ker2job9@gmail.com>
2022-07-21 16:38:05 +0900

    active_hashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :move_to_toppage
   def index
   end
   def new
+  end
+
+  private
+  def move_to_toppage
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "items#index"
-  resources :user, only: :index
-  resources :item, only: :index
+  root to: "users#index"
+  resources :user, only: :index do
+    resources :item, only: [:index, :new]
+  end
 end
