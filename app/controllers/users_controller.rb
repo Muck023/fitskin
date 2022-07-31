6dde70d60c3ac74a9a8fa86fdc1edcf4c4414f7c
Muck023 <ker2job9@gmail.com>
2022-07-31 23:29:11 +0900

    フロントの微調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @items = Item.where(user_id: params[:id])
+    @items = Item.where(user_id: params[:id]).order("created_at DESC")
   end
 
   def edit
