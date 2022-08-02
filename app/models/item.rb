7cd193692c7f66e2a02ff1eb3f6c4bebc4bafd50
Muck023 <ker2job9@gmail.com>
2022-08-02 18:18:03 +0900

    本番環境のビューエラー対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,6 @@ class Item < ApplicationRecord
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_name, presence: true
-  validates :price, numericality: { only_integer: true }
   validates :evaluation_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :comment_title, presence: true
 end
