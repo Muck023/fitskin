f2987cef651bfec6c1673208a24c547ad9945ee1
Muck023 <ker2job9@gmail.com>
2022-08-05 19:25:16 +0900

    SNS認証時の情報を保存するモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: :google_oauth2
 
   validates :usage_id, numericality: { only_integer: true, message: "を選択してください" }
 
+  has_many :sns_credentials
   has_many :items
   has_one_attached :profile_image
 
